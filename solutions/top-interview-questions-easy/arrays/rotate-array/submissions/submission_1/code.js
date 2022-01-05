37b4c7d0fe073c6c2a5aa8b60088f20382d65d13
Ihor Hadzera <ihor.hadzera@gmail.com>
2022-01-05 10:03:03 +0100

    Added array rotation

---
+++ solutions/top-interview-questions-easy/arrays/rotate-array/submissions/submission_1/code.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+ var rotate = function(nums, k) {
+    let kk = k % nums.length;
+    
+    let tail = nums.slice(kk * -1);
+    let head = nums.slice(0, nums.length-kk);
+    
+    for(let i=0;i<nums.length;i++){
+        if(i<kk){
+            nums[i]=tail[i];
+        } else {
+            nums[i]=head[i-kk]
+        }
+    }
+};
