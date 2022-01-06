b70b9874109b7b69cfa1b38f600176b68a5de68f
Ihor Hadzera <ihor.hadzera@gmail.com>
2022-01-06 14:53:53 +0100

    contains_duplicates

---
+++ solutions/top-interview-questions-easy/arrays/contains-duplicates/submissions/submission_1/code.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var containsDuplicate = function(nums) {
+    let dupAr = [];
+    for(let i=0; i<nums.length; i++) {
+        if (dupAr[nums[i]] === true) {
+            return true;
+        }
+        dupAr[nums[i]] = true;
+    }
+    
+    return false;
+};
