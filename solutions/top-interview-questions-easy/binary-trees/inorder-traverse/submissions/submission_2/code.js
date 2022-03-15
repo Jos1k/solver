264df3e26c91da06c007c7afae66739110a40bd1
Ihor Hadzera <ihor.hadzera@gmail.com>
2022-03-15 13:14:01 +0100

    iteratively inorder

---
+++ solutions/top-interview-questions-easy/binary-trees/inorder-traverse/submissions/submission_2/code.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    if(!root) return [];
+    
+    let result = [];
+    let stack = [];
+    let current = 0;
+    
+    current = root;
+    
+    while (current || stack.length){
+        while(current){
+            stack.push(current);
+            current = current.left
+        }
+        current = stack.pop();
+        result.push(current.val);
+        
+        current = current.right
+    }
+    
+        
+    return result;
+};
+
+module.exports = inorderTraversal;
