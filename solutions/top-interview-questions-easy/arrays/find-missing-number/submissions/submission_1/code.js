6f06451c96b65c5aa00b19289e974f90d66ba837
Ihor Hadzera <ihor.hadzera@gmail.com>
2022-05-13 08:59:23 +0200

    misisng number

--- solutions/top-interview-questions-easy/arrays/find-missing-number/submissions/submission_1/code.js
+++ solutions/top-interview-questions-easy/arrays/find-missing-number/submissions/submission_1/code.js
@@ -1,1 +1,15 @@
-get sum of values - get sum of indexes
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var missingNumber = function(nums) {
+    let sumIndex = 0, sumValues = 0;
+    nums.sort(); 
+    
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]!=i) return i;
+    }
+    return nums.length;
+};
+
+module.exports = missingNumber;
