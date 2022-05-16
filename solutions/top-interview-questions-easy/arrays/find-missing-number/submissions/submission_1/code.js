b96baef361dd93bb6579d1ae6eafa29704d9fab0
Ihor Hadzera <ihor.hadzera@gmail.com>
2022-05-16 08:26:10 +0200

    Added fix for missing number

--- solutions/top-interview-questions-easy/arrays/find-missing-number/submissions/submission_1/code.js
+++ solutions/top-interview-questions-easy/arrays/find-missing-number/submissions/submission_1/code.js
@@ -3,8 +3,7 @@
  * @return {number}
  */
  var missingNumber = function(nums) {
-    let sumIndex = 0, sumValues = 0;
-    nums.sort(); 
+    nums.sort((a,b) => a-b); 
     
     for(let i=0; i<nums.length; i++){
         if(nums[i]!=i) return i;
