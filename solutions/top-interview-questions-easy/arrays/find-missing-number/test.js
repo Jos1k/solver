51a05ed22ec0c83a60cc17c314af9609f449a73b
Ihor Hadzera <ihor.hadzera@gmail.com>
2022-05-16 08:51:43 +0200

    removed empty line

--- solutions/top-interview-questions-easy/arrays/find-missing-number/test.js
+++ solutions/top-interview-questions-easy/arrays/find-missing-number/test.js
@@ -1,7 +1,6 @@
 const missingNumber1 = require("./submissions/submission_1/code");
 const missingNumber2 = require("./submissions/submission_2/code");
 
-
 const input = [45,35,38,13,12,23,48,15,44,21,43,26,6,37,1,19,22,3,11,32,4,16,28,49,29,36,33,8,9,39,46,17,41,7,2,5,27,20,40,34,30,25,47,0,31,42,24,10,14];
 
 const res2 = missingNumber2(input);
