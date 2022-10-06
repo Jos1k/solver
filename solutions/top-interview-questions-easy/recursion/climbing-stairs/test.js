7ea08c1000e4dab7fb62e96e2990ef0bcd3f873b
Ihor Hadzera <ihor.hadzera@gmail.com>
2022-10-06 09:55:10 +0200

    added test

---
+++ solutions/top-interview-questions-easy/recursion/climbing-stairs/test.js
@@ -0,0 +1,6 @@
+const climbStairs = require("./submissions/submission_1/code");
+
+const stairsCount = 5;
+const result = climbStairs(stairsCount);
+
+console.log(`Result of climbing to ${stairsCount} is: ${result}`);
